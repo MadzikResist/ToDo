5e88c25c77daf4982eaa78263d45cf615f20de37
Magdalena Szpitalna <madzikresist@gmail.com>
2022-08-29 12:28:16 +0200

    dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,13 +12,9 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
 
-  @media (min-width: 769px) {
+  @media (min-width: 1200px) {
     height: 100vh;
     background: url(${background2}) center/cover no-repeat;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
   }
 `;
 const ListContainer = styled.div`
@@ -31,7 +27,7 @@ const ListContainer = styled.div`
   flex-direction: column;
   align-items: center;
 
-  @media (min-width: 769px) {
+  @media (min-width: 1200px) {
     width: 80%;
     height: 80%;
     display: flex;
@@ -44,9 +40,7 @@ const Text = styled.h1`
   font-size: 1.8rem;
   word-wrap: break-word;
 
-  @media (min-width: 769px) {
-    text-align: center;
-    color: white;
+  @media (min-width: 1200px) {
     font-size: 3rem;
   }
 `;
@@ -58,26 +52,33 @@ const Title = styled.h2`
   word-wrap: break-word;
   font-size: 1.6rem;
 
-  @media (min-width: 769px) {
-    margin: 0;
-    width: 20%;
+  @media (min-width: 1200px) {
+    width: 90%;
   }
 `;
 const Date = styled.p`
   width: 10%;
+  margin: 16px 0 0 0;
+  padding: 0;
+
+  @media (min-width: 1200px) {
+    width: 100%;
+  }
 `;
 const TextToDo = styled.p`
   width: 100%;
   word-wrap: break-word;
 
-  @media (min-width: 769px) {
-    width: 40%;
+  @media (min-width: 1200px) {
+    margin: 0;
+    padding: 0;
   }
 `;
 const CheckDone = styled.input`
   width: 24px;
   height: 24px;
-
+  padding: 0;
+  margin: 0;
   -webkit-appearance: none;
   -moz-appearance: none;
   -o-appearance: none;
@@ -127,14 +128,9 @@ const List = styled.div`
   margin-bottom: 16px;
   align-items: center;
   padding: 8px;
-  @media (min-width: 769px) {
+  @media (min-width: 1200px) {
     width: 90%;
-    height: 80%;
-    display: flex;
-    flex-direction: column;
     margin-bottom: 16px;
-    align-items: center;
-    padding: 8px;
   }
 `;
 const ListElement = styled.div`
@@ -146,16 +142,11 @@ const ListElement = styled.div`
   margin-bottom: 16px;
   padding: 8px;
   flex-direction: column;
-  @media (min-width: 769px) {
+  @media (min-width: 1200px) {
     width: 95%;
-    height: auto;
-    display: flex;
     justify-content: space-between;
     border: 3px solid #ffd7b5;
-    margin-bottom: 16px;
     align-items: center;
-    padding: 8px;
-
     flex-direction: row;
   }
 `;
@@ -164,21 +155,15 @@ const Buttons = styled.div`
   justify-content: space-between;
   width: 100%;
   margin-top: 10%;
-  @media (min-width: 769px) {
-    display: flex;
-    justify-content: space-evenly;
-    width: 25%;
+  @media (min-width: 1200px) {
     margin-top: 0;
+    align-items: end;
   }
 `;
 const CheckBoxLabel = styled.label`
   display: inline-flex;
   cursor: pointer;
   position: relative;
-  padding-right: 16px;
-
-  @media (min-width: 769px) {
-  }
 `;
 const AddButton = styled.svg`
   width: 50px;
@@ -187,16 +172,28 @@ const AddButton = styled.svg`
   border: 3px solid #d2b79e;
   background: transparent;
   border-radius: 50%;
-
   cursor: pointer;
-  @media (min-width: 769px) {
+  @media (min-width: 1200px) {
     width: 60px;
     height: 60px;
-    fill: white;
-    border: 3px solid #d2b79e;
-    background: transparent;
-    border-radius: 50%;
-    cursor: pointer;
+  }
+`;
+const TitleDate = styled.div`
+  display: block;
+  width: 100%;
+  @media (min-width: 1200px) {
+    width: 40%;
+  }
+`;
+const TextButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  justify-content: space-between;
+  @media (min-width: 1200px) {
+    width: 60%;
+    height: 100%;
   }
 `;
 const Dashboard = () => {
@@ -206,41 +203,56 @@ const Dashboard = () => {
         <Text>Your To Do List</Text>
         <List>
           <ListElement style={{ marginTop: '24px' }}>
-            <Title>Titlegfdgdgfkfd;lgfdk;gfk;fdk;fdk;</Title>
-            <Date>20.20.2022</Date>
-            <TextToDo>
-              Musze przytulić swojego
-              chłopakfffffgkdfngdgfjdgjdfjgldkgjldfjldjlkgjdlgfjlfdjflgjlkjlja
-            </TextToDo>
-            <Buttons>
-              <CheckBoxLabel>
-                <CheckDone type="checkbox"></CheckDone>
-                <span style={{ color: 'black', padding: '0.5rem 0.25rem' }}>Done!</span>
-              </CheckBoxLabel>
-              <Edit xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z" />
-              </Edit>
-              <Delete xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z" />
-              </Delete>
-            </Buttons>
+            <TitleDate>
+              <Title>Titlegfdgdgfkfd;lgfdk;gfk;fdk;fdk;</Title>
+              <Date>20.20.2022</Date>
+            </TitleDate>
+            <TextButtons>
+              <TextToDo>
+                Musze przytulić swojego
+                chłopakfffffgkdfngdgfjdgjdfjgldkgjldfjldjlkgjdlgfjlfdjflgjlkjlja
+              </TextToDo>
+              <Buttons>
+                <CheckBoxLabel>
+                  <CheckDone type="checkbox"></CheckDone>
+                  <span style={{ color: 'black', padding: '0 0 0 8px', margin: '0 0 0 0' }}>
+                    Done!
+                  </span>
+                </CheckBoxLabel>
+                <Edit xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z" />
+                </Edit>
+                <Delete xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z" />
+                </Delete>
+              </Buttons>
+            </TextButtons>
           </ListElement>
-          <ListElement>
-            <Title>Title</Title>
-            <Date>20.20.2022</Date>
-            <TextToDo>Musze przytulić swojego chłopaka</TextToDo>
-            <Buttons>
-              <CheckBoxLabel>
-                <CheckDone type="checkbox"></CheckDone>
-                <span style={{ color: 'black', padding: '0.5rem 0.25rem' }}>Done!</span>
-              </CheckBoxLabel>
-              <Edit xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z" />
-              </Edit>
-              <Delete xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z" />
-              </Delete>
-            </Buttons>
+          <ListElement style={{ marginTop: '24px' }}>
+            <TitleDate>
+              <Title>Titlegfdgdgfkfd;lgfdk;gfk;fdk;fdk;</Title>
+              <Date>20.20.2022</Date>
+            </TitleDate>
+            <TextButtons>
+              <TextToDo>
+                Musze przytulić swojego
+                chłopakfffffgkdfngdgfjdgjdfjgldkgjldfjldjlkgjdlgfjlfdjflgjlkjlja
+              </TextToDo>
+              <Buttons>
+                <CheckBoxLabel>
+                  <CheckDone type="checkbox"></CheckDone>
+                  <span style={{ color: 'black', padding: '0 0 0 8px', margin: '0 0 0 0' }}>
+                    Done!
+                  </span>
+                </CheckBoxLabel>
+                <Edit xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path d="M421.7 220.3L188.5 453.4L154.6 419.5L158.1 416H112C103.2 416 96 408.8 96 400V353.9L92.51 357.4C87.78 362.2 84.31 368 82.42 374.4L59.44 452.6L137.6 429.6C143.1 427.7 149.8 424.2 154.6 419.5L188.5 453.4C178.1 463.8 165.2 471.5 151.1 475.6L30.77 511C22.35 513.5 13.24 511.2 7.03 504.1C.8198 498.8-1.502 489.7 .976 481.2L36.37 360.9C40.53 346.8 48.16 333.9 58.57 323.5L291.7 90.34L421.7 220.3zM492.7 58.75C517.7 83.74 517.7 124.3 492.7 149.3L444.3 197.7L314.3 67.72L362.7 19.32C387.7-5.678 428.3-5.678 453.3 19.32L492.7 58.75z" />
+                </Edit>
+                <Delete xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z" />
+                </Delete>
+              </Buttons>
+            </TextButtons>
           </ListElement>
         </List>
         <AddButton xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
--- src/index.js
+++ src/index.js
@@ -3,42 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
-  html {
-    
-    * {
-      scrollbar-width: auto;
-      scrollbar-color: #caa1d6 #58465e;
+    *{
+box-sizing: border-box;
     }
-
-    /* Chrome, Edge, and Safari */
-
-    *::-webkit-scrollbar {
-      width: 10px;
-    }
-
-    *::-webkit-scrollbar-track {
-      background: #58465e;
-    }
-
-    *::-webkit-scrollbar-thumb {
-      background-color: #caa1d6;
-      border-radius: 10px;
-      border: 0 none #ffffff;
-    }
-  }
-
   body {
-    padding: 0;
-    margin: 0;
-    overflow-x: hidden;
-    font-family: 'Mali', cursive;
-
-
-  }
-
-  *, * ::before, *::after {
-    box-sizing: border-box;
-  }
+      overflow-x: hidden;
+      font-family: 'Mali', cursive;
+    }
+    
 `;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
